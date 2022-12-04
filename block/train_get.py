1d13d7517a4d27055f7d754fd8f5a5997ef04d2f
twk <tanwk@knownsec.com>
2022-12-04 21:24:35 +0800

    !

--- block/train_get.py
+++ block/train_get.py
@@ -7,10 +7,12 @@ def train_get(args, dataset_dict, model_dict, loss):
     model = model_dict['model']
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     for epoch in range(args.epoch):
+        # 训练
         print('\n-----------------------------------------------')
         print('| 第{}轮 | 训练集:{} | 批量:{} | 学习率:{} |\n'
               .format(epoch + 1, len(dataset_dict['train']), args.batch, optimizer.defaults['lr']))
-        model.train().to(args.device)
+        model.train().to(args.device, non_blocking=args.latch)
+        train_loss = 0  # 记录训练损失
         train_dataloader = torch.utils.data.DataLoader(torch_dataset(args, dataset_dict['train']),
                                                        batch_size=args.batch, shuffle=True, drop_last=True,
                                                        pin_memory=args.latch)
@@ -19,24 +21,32 @@ def train_get(args, dataset_dict, model_dict, loss):
             true_batch = true_batch.to(args.device, non_blocking=args.latch)
             pred_batch = model(train_batch)
             loss_batch = loss(pred_batch, true_batch)
+            train_loss += loss_batch.item()
             optimizer.zero_grad()
             loss_batch.backward()
             optimizer.step()
+        train_loss = train_loss / (item + 1) / args.batch
         # 验证
-        accuracy, precision, recall, m_ap, val_loss = val_get(args, dataset_dict, model, loss)
+        val_loss, accuracy, precision, recall, m_ap = val_get(args, dataset_dict, model, loss)
         # 保存
         if m_ap > 0.8:
-            if m_ap > model_dict['m_ap'] or m_ap == model_dict['m_ap'] and val_loss < model_dict['val_loss']:
+            if m_ap > model_dict['val_m_ap'] or m_ap == model_dict['val_m_ap'] and val_loss < model_dict['val_loss']:
                 model_dict['model'] = model
-                model_dict['m_ap'] = m_ap
+                model_dict['epoch'] = epoch
+                model_dict['train_loss'] = train_loss
                 model_dict['val_loss'] = val_loss
-                model_dict['accuracy'] = accuracy
-                model_dict['precision'] = precision
-                model_dict['recall'] = recall
+                model_dict['val_m_ap'] = m_ap
+                model_dict['val_accuracy'] = accuracy
+                model_dict['val_precision'] = precision
+                model_dict['val_recall'] = recall
                 model_dict['bgr_mean'] = args.bgr_mean
                 torch.save(model_dict, args.save_name.split('.')[0] + '.pt')
-                print('\n| 保存模型:{} | m_ap:{:.4f} | val_loss:{:.4f} |\n'
-                      .format(args.save_name.split('.')[0] + '.pt', m_ap, val_loss))
+                print('\n| 保存模型:{} | val_loss:{:.4f} | m_ap:{:.4f} |\n'
+                      .format(m_ap, val_loss, args.save_name.split('.')[0] + '.pt'))
+        # wandb
+        if args.wandb:
+            args.wandb_run.log({'epoch': epoch, 'train_loss': train_loss, 'val_loss': val_loss, 'val_m_ap': m_ap,
+                                'val_accuracy': accuracy, 'val_precision': precision, 'val_recall': recall})
     return model_dict
 
 
--- block/val_get.py
+++ block/val_get.py
@@ -5,23 +5,23 @@ from block.metric_get import metric
 
 def val_get(args, dataset_dict, model, loss):
     with torch.no_grad():
-        model.eval().to(args.device)
+        model.eval().to(args.device, non_blocking=args.latch)
         torch.cuda.empty_cache()
         val_dataloader = torch.utils.data.DataLoader(torch_dataset(args, dataset_dict['val']), batch_size=args.batch,
                                                      shuffle=False, drop_last=False, pin_memory=args.latch)
-        pred_all = []
-        true_all = []
+        val_pred = []
+        val_true = []
         for item, (val_batch, true_batch) in enumerate(tqdm.tqdm(val_dataloader)):
             val_batch = val_batch.to(args.device, non_blocking=args.latch)
-            pred_all.extend(model(val_batch).detach().cpu())
-            true_all.extend(true_batch.detach().cpu())
-        pred_all = torch.stack(pred_all, axis=0)
-        true_all = torch.stack(true_all, axis=0)
-        loss_all = loss(pred_all, true_all)
-        accuracy, precision, recall, m_ap = metric(pred_all, true_all)
+            val_pred.extend(model(val_batch).detach().cpu())
+            val_true.extend(true_batch.detach().cpu())
+        val_pred = torch.stack(val_pred, axis=0)
+        val_true = torch.stack(val_true, axis=0)
+        val_loss = loss(val_pred, val_true) / len(val_pred)
+        accuracy, precision, recall, m_ap = metric(val_pred, val_true)
         print('\n| 验证集:{} | loss:{:.4f} | accuracy:{:.4f} | precision:{:.4f} | recall:{:.4f} | m_ap:{:.4f} |\n'
-              .format(len(dataset_dict['val']), loss_all, accuracy, precision, recall, m_ap))
-    return accuracy, precision, recall, m_ap, loss_all
+              .format(len(dataset_dict['val']), val_loss, accuracy, precision, recall, m_ap))
+    return val_loss, accuracy, precision, recall, m_ap
 
 
 class torch_dataset(torch.utils.data.Dataset):
