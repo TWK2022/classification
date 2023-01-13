67a09f8bba42336fc2aa34d948d34bc5e6f952d7
twk <tanwk@knownsec.com>
2023-01-13 12:23:31 +0800

    !

--- tool/make_txt.py
+++ tool/make_txt.py
@@ -5,7 +5,7 @@ import argparse
 # 设置
 parser = argparse.ArgumentParser(description='将文件夹中的图片按比例添加到.txt标签中')
 parser.add_argument('--data_path', default=r'D:\dataset\classification\mask\image\mask', type=str, help='|图片所在目录|')
-parser.add_argument('--add', default=' 0', type=str, help='|标签内容为[图片相对路径+add]|')
+parser.add_argument('--add', default=' 0', type=str, help='|标签内容为[图片绝对路径+add]|')
 parser.add_argument('--divide', default='9,1', type=str, help='|图片划分到train.txt和val.txt的比例|')
 args = parser.parse_args()
 
