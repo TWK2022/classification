4643cd236b44193d0df8e3fa4e3e2dc42ec1cc6f
twk <tanwk@knownsec.com>
2023-01-13 22:06:54 +0800

    !

--- block/data_get.py
+++ block/data_get.py
@@ -12,28 +12,18 @@ class data_prepare(object):
 
     def _load(self):
         data_dict = {}
-        data_dict['train'] = self._load_train()
-        data_dict['val'] = self._load_val()
+        data_dict['train'] = self._load_label('train.txt')
+        data_dict['val'] = self._load_label('val.txt')
         data_dict['class'] = self._load_class()
         return data_dict
 
-    def _load_train(self):
-        with open(self.args.data_path + '/' + 'train.txt', encoding='utf-8')as f:
-            txt = [_.strip().split(' ') for _ in f.readlines()]
-        data_list = [[0, 0] for _ in range(len(txt))]
+    def _load_label(self, txt_name):
+        with open(self.args.data_path + '/' + txt_name, encoding='utf-8')as f:
+            txt = [_.strip().split(' ') for _ in f.readlines()]  # 读取所有图片路径和类别号
+        data_list = [[0, 0] for _ in range(len(txt))]  # [图片路径,类别独热编码]
         for i in range(len(txt)):
-            data_list[i][0] = self.args.data_path + '/image' + txt[i][0].split('image')[-1]
-            data_list[i][1] = np.zeros(self.args.output_class, dtype=np.float32)
-            for j in txt[i][1:]:
-                data_list[i][1][int(j)] = 1
-        return data_list
-
-    def _load_val(self):
-        with open(self.args.data_path + '/' + 'val.txt', encoding='utf-8')as f:
-            txt = [_.strip().split(' ') for _ in f.readlines()]
-        data_list = [[0, 0] for _ in range(len(txt))]
-        for i in range(len(txt)):
-            data_list[i][0] = self.args.data_path + '/image' + txt[i][0].split('image')[-1]
+            image_path = self.args.data_path + '/image' + txt[i][0].split('image')[-1]
+            data_list[i][0] = image_path
             data_list[i][1] = np.zeros(self.args.output_class, dtype=np.float32)
             for j in txt[i][1:]:
                 data_list[i][1][int(j)] = 1
