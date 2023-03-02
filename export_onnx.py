e9ba8bc8390f6ed7a5548437d291f8153cd118bd
twk <1024565378@qq.com>
2023-03-02 23:20:18 +0800

    !

--- export_onnx.py
+++ export_onnx.py
@@ -6,7 +6,7 @@ import argparse
 # 设置
 parser = argparse.ArgumentParser(description='将pt模型字典中的模型转为onnx，同时导出类别信息')
 parser.add_argument('--weight', default='best.pt', type=str, help='|模型位置|')
-parser.add_argument('--input_size', default=160, type=int, help='|输入图片大小|')
+parser.add_argument('--input_size', default=640, type=int, help='|输入图片大小|')
 parser.add_argument('--batch', default=0, type=int, help='|输入图片批量，0为动态|')
 parser.add_argument('--sim', default=True, type=bool, help='|使用onnxsim压缩简化模型|')
 parser.add_argument('--float16', default=True, type=bool, help='|转换的onnx模型数据类型，需要GPU，False时为float32|')
