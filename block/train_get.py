24216ef08add59f82a0ab8ef0880b5a6c2f7eda2
twk <1024565378@qq.com>
2023-03-21 18:41:54 +0800

    !

--- block/train_get.py
+++ block/train_get.py
@@ -16,12 +16,17 @@ def train_get(args, data_dict, model_dict, loss):
     val_dataloader = torch.utils.data.DataLoader(torch_dataset(args, 'val', data_dict['val'], data_dict['class']),
                                                  batch_size=args.batch, shuffle=False, drop_last=False,
                                                  pin_memory=args.latch, num_workers=args.num_worker)
+    # wandb
+    if args.wandb:
+        wandb_image_list = []  # 记录所有的wandb_image最后一起添加(最多添加args.wandb_image_num张)
     for epoch in range(args.epoch):
         # 训练
         print(f'\n-----------------------第{epoch + 1}轮-----------------------')
         model.train()
         train_loss = 0  # 记录训练损失
         for item, (image_batch, true_batch) in enumerate(tqdm.tqdm(train_dataloader)):
+            wandb_image_batch = image_batch.cpu().numpy().astype(np.uint8) \
+                if args.wandb and len(wandb_image_list) < args.wandb_image_num else None
             image_batch = image_batch.to(args.device, non_blocking=args.latch)
             true_batch = true_batch.to(args.device, non_blocking=args.latch)
             if args.scaler:
@@ -40,6 +45,19 @@ def train_get(args, data_dict, model_dict, loss):
                 optimizer.step()
             # 记录损失
             train_loss += loss_batch.item()
+            # wandb
+            if args.wandb and len(wandb_image_list) < args.wandb_image_num:
+                cls = true_batch.cpu().numpy().tolist()
+                for i in range(len(wandb_image_batch)):  # 遍历每一张图片
+                    image = wandb_image_batch[i]
+                    text = ['{:.2f}'.format(_) for _ in cls[i]]
+                    text = text[0] if len(text) == 1 else '--'.join(text)
+                    cv2.putText(image, text, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    wandb_image = wandb.Image(image)
+                    wandb_image_list.append(wandb_image)
+                    if len(wandb_image_list) == args.wandb_image_num:
+                        args.wandb_run.log({f'image/train_image': wandb_image_list})
+                        break
         train_loss = train_loss / (item + 1)
         print('\n| 训练集:{} | train_loss:{:.4f} |\n'.format(len(data_dict['train']), train_loss))
         # 清理显存空间
@@ -48,19 +66,20 @@ def train_get(args, data_dict, model_dict, loss):
         # 验证
         val_loss, accuracy, precision, recall, m_ap = val_get(args, val_dataloader, model, loss)
         # 保存
-        if m_ap > 0.8:
-            if m_ap > model_dict['val_m_ap'] or m_ap == model_dict['val_m_ap'] and val_loss < model_dict['val_loss']:
-                model_dict['model'] = model
-                model_dict['class'] = data_dict['class']
-                model_dict['epoch'] = epoch
-                model_dict['train_loss'] = train_loss
-                model_dict['val_loss'] = val_loss
-                model_dict['val_accuracy'] = accuracy
-                model_dict['val_precision'] = precision
-                model_dict['val_recall'] = recall
-                model_dict['val_m_ap'] = m_ap
-                torch.save(model_dict, args.save_name)
-                print('\n| 保存模型:{} | m_ap:{:.4f} |\n'.format(args.save_name, m_ap))
+        standard = model_dict['val_m_ap']
+        model_dict['model'] = model
+        model_dict['epoch'] = epoch
+        model_dict['class'] = data_dict['class']
+        model_dict['train_loss'] = train_loss
+        model_dict['val_loss'] = val_loss
+        model_dict['val_accuracy'] = accuracy
+        model_dict['val_precision'] = precision
+        model_dict['val_recall'] = recall
+        model_dict['val_m_ap'] = m_ap
+        torch.save(model_dict, 'last.pt')  # 保存最后一次训练的模型
+        if m_ap > 0.5 and m_ap > standard:
+            torch.save(model_dict, args.save_name)  # 保存最佳模型
+            print('\n| 保存最佳模型:{} | val_m_ap:{:.4f} |\n'.format(args.save_name, m_ap))
         # wandb
         if args.wandb:
             args.wandb_run.log({'metric/train_loss': train_loss,
@@ -85,14 +104,6 @@ class torch_dataset(torch.utils.data.Dataset):
             albumentations.LongestMaxSize(args.input_size),
             albumentations.PadIfNeeded(min_height=args.input_size, min_width=args.input_size,
                                        border_mode=cv2.BORDER_CONSTANT, value=(127, 127, 127))])
-        # wandb可视化部分
-        self.wandb = args.wandb
-        if self.wandb:
-            self.wandb_run = args.wandb_run
-            self.wandb_count = 0  # 用于限制添加的图片数量(最多添加args.wandb_image_num张)
-            self.wandb_image_num = args.wandb_image_num
-            self.wandb_image = []  # 记录所有的image最后一起添加
-            self.class_name = class_name
 
     def __len__(self):
         return len(self.data)
@@ -105,17 +116,4 @@ class torch_dataset(torch.utils.data.Dataset):
         image = self.transform(image=image)['image']  # 缩放和填充图片
         image = torch.tensor(image, dtype=torch.float32)  # 转换为tensor(归一化、减均值、除以方差、调维度等在模型中完成)
         label = torch.tensor(self.data[index][1], dtype=torch.float32)  # 转换为tensor
-        # 使用wandb添加图片
-        if self.wandb and self.wandb_count < self.wandb_image_num:
-            self.wandb_count += 1
-            text = ''
-            for i in range(len(label)):
-                text += str(int(label[i].item())) + '-'
-            text = text[:-1]
-            wandb_image = np.array(image, dtype=np.uint8)
-            cv2.putText(wandb_image, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            wandb_image = wandb.Image(wandb_image)
-            self.wandb_image.append(wandb_image)
-            if self.wandb_count == self.wandb_image_num:
-                self.wandb_run.log({f'image/{self.tag}_image': self.wandb_image})
         return image, label
