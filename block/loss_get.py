6a75f0d58a894d16e78ce6c1ac40222badc4942f
twk <1024565378@qq.com>
2023-03-31 16:12:34 +0800

    !

--- block/loss_get.py
+++ block/loss_get.py
@@ -2,7 +2,6 @@ import torch
 
 
 def loss_get(args):
-    with torch.no_grad():
-        choice_dict = {'bce': 'torch.nn.BCEWithLogitsLoss()'}
-        loss = eval(choice_dict[args.loss])
+    choice_dict = {'bce': 'torch.nn.BCEWithLogitsLoss()'}
+    loss = eval(choice_dict[args.loss])
     return loss
