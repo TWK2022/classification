dd604b8a524e4208ba866f3c0bc8d6cfe68e4622
TWK <109967047+TWK2022@users.noreply.github.com>
2024-01-03 11:04:19 +0800

    !

--- block/train_get.py
+++ block/train_get.py
@@ -45,9 +45,10 @@ def train_get(args, data_dict, model_dict, loss):
         # 训练
         print(f'\n-----------------------第{epoch}轮-----------------------') if args.local_rank == 0 else None
         model.train()
-        train_loss = 0  # 记录训练损失
-        tqdm_show = tqdm.tqdm(total=len(data_dict['train']) // args.batch // args.device_number * args.device_number,
-                              postfix=dict, mininterval=0.2) if args.local_rank == 0 else None  # tqdm
+        train_loss = 0  # 记录损失
+        if args.local_rank == 0:  # tqdm
+            tqdm_len = len(data_dict['train']) // args.batch // args.device_number * args.device_number
+            tqdm_show = tqdm.tqdm(total=tqdm_len, mininterval=0.2)
         for index, (image_batch, true_batch) in enumerate(train_dataloader):
             if args.wandb and args.local_rank == 0 and len(wandb_image_list) < args.wandb_image_num:
                 wandb_image_batch = (image_batch * 255).cpu().numpy().astype(np.uint8).transpose(0, 2, 3, 1)
@@ -89,10 +90,12 @@ def train_get(args, data_dict, model_dict, loss):
                     if len(wandb_image_list) == args.wandb_image_num:
                         break
         # tqdm
-        tqdm_show.close() if args.local_rank == 0 else None
+        if args.local_rank == 0:
+            tqdm_show.close()
         # 计算平均损失
         train_loss = train_loss / (index + 1)
-        print('\n| train_loss:{:.4f} | lr:{:.6f} |\n'.format(train_loss, optimizer.param_groups[0]['lr']))
+        if args.local_rank == 0:
+            print('\n| train_loss:{:.4f} | lr:{:.6f} |\n'.format(train_loss, optimizer.param_groups[0]['lr']))
         # 调整学习率
         optimizer = optimizer_adjust(optimizer, epoch + 1, train_loss)
         # 清理显存空间
