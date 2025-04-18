8efce9f44b557824ef817c7d6bf5f71659fb3b06
TWK <109967047+TWK2022@users.noreply.github.com>
2025-04-18 16:44:03 +0800

    !

--- model/timm_model.py
+++ model/timm_model.py
@@ -6,9 +6,10 @@ from model.layer import linear_head
 class timm_model(torch.nn.Module):
     def __init__(self, args):
         super().__init__()
-        self.backbone = timm.create_model(args.model, in_chans=3, features_only=True, exportable=True)
-        out_dim = self.backbone.feature_info.channels()[-1]  # backbone输出有多个，接最后一个输出，并得到其通道数
-        self.linear_head = linear_head(out_dim, args.output_class)
+        default_model = 'efficientnetv2_m'
+        self.backbone = timm.create_model(default_model, in_chans=3, features_only=True, exportable=True)
+        output_dim = self.backbone.feature_info.channels()[-1]  # backbone输出有多个，接最后一个输出，并得到其通道数
+        self.linear_head = linear_head(output_dim, args.output_class)
 
     def forward(self, x):
         x = self.backbone(x)
@@ -20,7 +21,6 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--model', default='efficientnetv2_s', type=str)
     parser.add_argument('--input_size', default=320, type=int)
     parser.add_argument('--output_class', default=2, type=int)
     args = parser.parse_args()
